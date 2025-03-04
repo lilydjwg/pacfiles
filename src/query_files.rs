72ef1420b905c0b7919756c74524933835560546
lilydjwg <lilydjwg@gmail.com>
2025-03-04 14:08:23 +0800

    fix querying a full path is different that pacman -F

--- src/query_files.rs
+++ src/query_files.rs
@@ -49,6 +49,7 @@ fn query_files_regex(
       Path::new(&path).file_stem().unwrap().to_str().unwrap(),
       quiet,
       installed,
+      None,
     )
   })
 }
@@ -59,12 +60,25 @@ fn query_files_pattern(
   installed: &InstalledPackages,
 ) -> IoResult<()> {
   let mut stdout = stdout().lock();
+  let is_fullpath = pattern.contains('/');
+  let is_glob = pattern.contains(['*', '?', '[', ']']);
+  let mut validating_path = None;
   let mut modified_pattern = String::new();
-  let p = if !pattern.contains(['*', '?', '[', ']']) {
+  let p = if !is_fullpath && !is_glob {
     modified_pattern.push('[');
     modified_pattern.push(pattern.chars().next().unwrap());
     modified_pattern.push(']');
     modified_pattern.push_str(&pattern[1..]);
+    modified_pattern.as_str()
+  } else if is_fullpath {
+    modified_pattern.push_str("*/");
+    if let Some(stripped) = pattern.strip_prefix('/') {
+      modified_pattern.push_str(stripped);
+      validating_path = Some(stripped);
+    } else {
+      modified_pattern.push_str(pattern);
+      validating_path = Some(pattern);
+    }
     modified_pattern.as_str()
   } else {
     pattern
@@ -77,6 +91,7 @@ fn query_files_pattern(
       Path::new(&path).file_stem().unwrap().to_str().unwrap(),
       quiet,
       installed,
+      validating_path,
     )
   })
 }
@@ -87,6 +102,7 @@ fn output_plocate(
   repo: &str,
   quiet: bool,
   installed: &InstalledPackages,
+  validating_path: Option<&str>,
 ) -> IoResult<()> {
   let mut last_pkgname = String::new();
   for pf in plocate {
@@ -96,34 +112,48 @@ fn output_plocate(
     if same_pkgname && quiet {
       continue;
     }
+    let path = pf.path();
+    if let Some(p) = validating_path {
+      if !path.starts_with(p) {
+        continue;
+      }
+    }
     if quiet {
       writeln!(stdout, "{}/{}", repo, pkgname)?;
     } else {
-      let path = pf.path();
-      if !same_pkgname {
-        let version = pf.version();
-        let installed_version = installed.package_version(pkgname);
-        if let Some(iv) = installed_version {
-          let installed_text = if iv == version {
-            Cow::Borrowed("[installed]")
+      let version = pf.version();
+      if validating_path.is_some() {
+        writeln!(stdout, "{} is owned by {}{} {}",
+          path,
+          Color::Magenta.bold().paint(format!("{repo}/")),
+          Style::new().bold().paint(pkgname),
+          Color::Green.bold().paint(version),
+        )?;
+      } else {
+        if !same_pkgname {
+          let installed_version = installed.package_version(pkgname);
+          if let Some(iv) = installed_version {
+            let installed_text = if iv == version {
+              Cow::Borrowed("[installed]")
+            } else {
+              Cow::Owned(format!("[installed: {iv}]"))
+            };
+            writeln!(stdout, "{}{} {} {}",
+              Color::Magenta.bold().paint(format!("{repo}/")),
+              Style::new().bold().paint(pkgname),
+              Color::Green.bold().paint(version),
+              Color::Cyan.bold().paint(installed_text),
+            )?;
           } else {
-            Cow::Owned(format!("[installed: {iv}]"))
-          };
-          writeln!(stdout, "{}{} {} {}",
-            Color::Magenta.bold().paint(format!("{repo}/")),
-            Style::new().bold().paint(pkgname),
-            Color::Green.bold().paint(version),
-            Color::Cyan.bold().paint(installed_text),
-          )?;
-        } else {
-          writeln!(stdout, "{}{} {}",
-            Color::Magenta.bold().paint(format!("{repo}/")),
-            Style::new().bold().paint(pkgname),
-            Color::Green.bold().paint(version),
-          )?;
+            writeln!(stdout, "{}{} {}",
+              Color::Magenta.bold().paint(format!("{repo}/")),
+              Style::new().bold().paint(pkgname),
+              Color::Green.bold().paint(version),
+            )?;
+          }
         }
+        writeln!(stdout, "    {}", path)?;
       }
-      writeln!(stdout, "    {}", path)?;
     }
     if !same_pkgname {
       last_pkgname.clear();
